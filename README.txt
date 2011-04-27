a22087ce23aeaee5b1463b69b57aaf1d90e8c832
Muharem Hrnjadovic <mh@foldr3.com>
2011-04-27 14:50:44 +0200

    throw away django project, we need an application

--- README.txt
+++
@@ -1,3 +0,0 @@
-This is the repository with the OpenQuake django application utilised by the OpenQuake OpenGeo UI.
-
-For more detail on the API please see: https://github.com/gem/openquake/wiki/demo-client-API
--- ui/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-import simplejson
-
-def upload(request):
-    result = dict(
-        status="success", msg="Model upload successful",
-        ruptures=[2, 3, 4], sources=[2,3,4])
-    return HttpResponse(simplejson.dumps(result))
