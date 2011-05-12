c8b267db14b40689a51c0b262d0d5f6ac608ac25
Muharem Hrnjadovic <mh@foldr3.com>
2011-05-12 05:53:25 +0200

    resurrected the run_cmd() function

---
+++ geonode/tests/mtapi_utils_unittests.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2010-2011, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenQuake.  If not, see
+# <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+
+"""
+Unit tests for the geonode/mtapi/utils.py module.
+"""
+
+
+import unittest
+
+from geonode.mtapi.utils import run_cmd
+
+
+class RunCmdTestCase(unittest.TestCase):
+    """Tests the behaviour of dbmaint.run_cmd()."""
+
+    def test_run_cmd_with_success(self):
+        """Invoke a command without errors."""
+        code, out, err = run_cmd(["echo", "-n", "Hello world!"])
+        self.assertEqual(0, code)
+        self.assertEqual("Hello world!", out)
+        self.assertEqual("", err)
+
+    def test_run_cmd_with_errors(self):
+        """Invoke a command with errors."""
+        try:
+            code, out, err = run_cmd(["ls", "-AF", "/this/does/not/exist"])
+        except Exception, e:
+            self.assertEqual(
+                "ls terminated with exit code: 2\nls: cannot access "
+                "/this/does/not/exist: No such file or directory\n", e.args[0])
+        else:
+            raise Exception("exception not raised")
+
+    def test_run_cmd_with_errors_and_ignore_exit_code(self):
+        """Invoke a command with errors but ignore the exit code."""
+        code, out, err = run_cmd(
+            ["ls", "-AF", "/this/does/not/exist"], ignore_exit_code=True)
+        self.assertEqual(2, code)
+        self.assertEqual("", out)
+        self.assertEqual("ls: cannot access /this/does/not/exist: No such "
+                         "file or directory\n", err)
