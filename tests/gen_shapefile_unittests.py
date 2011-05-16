9463857a43f1bebb4b10ef488deeb19b71a18a2b
Muharem Hrnjadovic <mh@foldr3.com>
2011-05-16 09:17:02 +0200

    tests for tag_extractor()

--- tests/gen_shapefile_unittests.py
+++ tests/gen_shapefile_unittests.py
@@ -23,10 +23,9 @@ Unit tests for the bin/generate_shapefile.py tool.
 """
 
 
-import os
 import unittest
 
-from bin.generate_shapefile import extract_position
+from bin.generate_shapefile import extract_position, tag_extractor
 
 
 class ExtractPositionTestCase(unittest.TestCase):
@@ -72,3 +71,105 @@ class ExtractPositionTestCase(unittest.TestCase):
                              "for position -121.8 37.5", e.args[0])
         else:
             raise Exception("No exception raised!")
+
+
+class TagExtractorTestCase(unittest.TestCase):
+    """Tests the behaviour of generate_shapefile.tag_extractor()."""
+
+    def test_tag_extractor_with_hazard_map(self):
+        """
+        `HMNode` tags are extracted correctly.
+        """
+        expected_data = ['''<HMNode gml:id="n_1">
+        <HMSite>
+          <gml:Point srsName="epsg:4326">
+            <gml:pos>-121.8 37.9</gml:pos>
+          </gml:Point>
+          <vs30>760.0</vs30>
+        </HMSite>
+        <IML>1.23518683436</IML>
+      </HMNode>''', '''<HMNode gml:id="n_2">
+        <HMSite>
+          <gml:Point srsName="epsg:4326">
+            <gml:pos>-122.0 37.5</gml:pos>
+          </gml:Point>
+          <vs30>760.0</vs30>
+        </HMSite>
+        <IML>1.19244541041</IML>
+      </HMNode>''', '''<HMNode gml:id="n_3">
+        <HMSite>
+          <gml:Point srsName="epsg:4326">
+            <gml:pos>-122.1 38.0</gml:pos>
+          </gml:Point>
+          <vs30>760.0</vs30>
+        </HMSite>
+        <IML>1.1905288226</IML>
+      </HMNode>''']
+
+        for idx, hmnode in enumerate(tag_extractor(
+            "HMNode", "tests/hazardmap-0.1-quantile-0.25.xml")):
+            self.assertEqual(expected_data[idx], hmnode)
+
+    def test_tag_extractor_with_loss_map(self):
+        """
+        `LMNode` tags are extracted correctly.
+        """
+        expected_data = ['''<LMNode gml:id="lmn_1">
+        <site>
+          <gml:Point srsName="epsg:4326">
+            <gml:pos>-118.229726 34.050622</gml:pos>
+          </gml:Point>
+        </site>
+        <loss xmlns:ns0="http://openquake.org/xmlns/nrml/0.2" ns0:assetRef="0">
+          <ns0:mean>1.71451736293</ns0:mean>
+          <ns0:stdDev>2.00606841051</ns0:stdDev>
+        </loss>
+        <loss xmlns:ns1="http://openquake.org/xmlns/nrml/0.2" ns1:assetRef="1">
+          <ns1:mean>14.3314083523</ns1:mean>
+          <ns1:stdDev>11.9001178481</ns1:stdDev>
+        </loss>
+        <loss xmlns:ns2="http://openquake.org/xmlns/nrml/0.2" ns2:assetRef="2">
+          <ns2:mean>0.00341983323202</ns2:mean>
+          <ns2:stdDev>0.0218042708753</ns2:stdDev>
+        </loss>
+      </LMNode>''', '''<LMNode gml:id="lmn_2">
+        <site>
+          <gml:Point srsName="epsg:4326">
+            <gml:pos>-118.241243 34.061557</gml:pos>
+          </gml:Point>
+        </site>
+        <loss xmlns:ns3="http://openquake.org/xmlns/nrml/0.2" ns3:assetRef="104">
+          <ns3:mean>260.1793321</ns3:mean>
+          <ns3:stdDev>298.536543258</ns3:stdDev>
+        </loss>
+        <loss xmlns:ns4="http://openquake.org/xmlns/nrml/0.2" ns4:assetRef="105">
+          <ns4:mean>205.54063128</ns4:mean>
+          <ns4:stdDev>182.363531209</ns4:stdDev>
+        </loss>
+        <loss xmlns:ns5="http://openquake.org/xmlns/nrml/0.2" ns5:assetRef="106">
+          <ns5:mean>163.603304574</ns5:mean>
+          <ns5:stdDev>222.371828022</ns5:stdDev>
+        </loss>
+      </LMNode>''', '''<LMNode gml:id="lmn_3">
+        <site>
+          <gml:Point srsName="epsg:4326">
+            <gml:pos>-118.245388 34.055984</gml:pos>
+          </gml:Point>
+        </site>
+        <loss xmlns:ns6="http://openquake.org/xmlns/nrml/0.2" ns6:assetRef="219">
+          <ns6:mean>59.1595800341</ns6:mean>
+          <ns6:stdDev>53.5693102791</ns6:stdDev>
+        </loss>
+        <loss xmlns:ns7="http://openquake.org/xmlns/nrml/0.2" ns7:assetRef="220">
+          <ns7:mean>104.689400653</ns7:mean>
+          <ns7:stdDev>65.9931553211</ns7:stdDev>
+        </loss>
+        <loss xmlns:ns8="http://openquake.org/xmlns/nrml/0.2" ns8:assetRef="221">
+          <ns8:mean>82.1438713787</ns8:mean>
+          <ns8:stdDev>156.848140719</ns8:stdDev>
+        </loss>
+      </LMNode>''']
+
+        for idx, hmnode in enumerate(tag_extractor(
+            "LMNode", "tests/loss-map-0fcfdbc7.xml")):
+            self.assertEqual(expected_data[idx], hmnode)
